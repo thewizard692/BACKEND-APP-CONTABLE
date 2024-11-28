81e4eb39c7e13fe4979a2be74544342e44c9f1ea
Javier Yepez <javier_yepez@outlook.com>
2024-11-28 12:38:59 -0600

    Cotizacion agregada funcional

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,13 @@
 import { Router } from 'express'
 import empleadosRoutes from './empleadoRoutes.js'
 import authRoutes from './authRoutes.js'
+import cotizacionRoutes from './CotizacionRoutes.js'
 
 const router = Router()
 
 //app necesito una ruta base 
 router.use('/empleados', empleadosRoutes)
 router.use('/auth', authRoutes)
+router.use('/cotizaciones', cotizacionRoutes);
 
 export default router

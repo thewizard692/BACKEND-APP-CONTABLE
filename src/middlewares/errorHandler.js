4c67a59a6b66c5b2c6012337b494b2d157fb1e5c
Jared0103 <josejared8ac@hotmail.com>
2024-12-06 19:28:26 -0600

    preparo el main

--- src/middlewares/errorHandler.js
+++
@@ -1,8 +0,0 @@
-const errorHandler = (err, req, res, next) => {
-  res.status(500).json({
-    message: 'Ocurrio un error en el servidor',
-    error: err.stack
-  })
-}
-
-export default errorHandler
